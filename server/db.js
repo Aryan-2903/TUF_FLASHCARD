704a72aab996020e5f06190b2068204464835a86
Aryan-2903 <134300592+Aryan-2903@users.noreply.github.com>
2024-08-13 18:04:31 +0530

    Update db.js

--- server/db.js
+++ server/db.js
@@ -1,13 +1,17 @@
-import mysql from 'mysql2';
-
-const pool = mysql.createPool({
-  host: '127.0.0.1',
-  user: 'root',
-  password: 'Aryan@123',
-  database: 'tuf',
-  waitForConnections: true,
-  connectionLimit: 10,
-  queueLimit: 0
+const mysql = require("mysql2");
+const JSON=require("JSON")
+require("dotenv").config();
+connection = mysql.createConnection({
+  host: process.env.MYSQL_HOST,
+  port: process.env.MYSQL_PORT,
+  user: process.env.MYSQL_USER,
+  password: process.env.MYSQL_PASSWORD,
+  database: process.env.MYSQL_DB,
+});
+connection.connect((err) => {
+    if (err) {
+        console.error("CONNECT FAILED", err.code);
+    } else console.log("CONNECTED");
 });
 
-export default pool.promise();
+module.exports = { connection };
